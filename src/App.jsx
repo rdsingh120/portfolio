c00c2c63f70f0592e4b60d11a8f2e4a85bd642da
Ripudaman Singh <rdsingh120@gmail.com>
2024-09-08 03:23:58 -0400

    fixed css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import Banner from "./components/Banner/Banner"
-import Navbar from "./components/Navbar/Navbar"
-import Sidebar from "./components/Sidebar/Sidebar"
+import Banner from './components/Banner/Banner'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
-
   return (
     <>
       <Navbar />
+      <Sidebar />
       <div className="container">
-        <Sidebar />
         <main>
           <Banner />
         </main>
