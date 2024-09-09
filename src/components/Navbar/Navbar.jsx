62bc7ddec4c42654d3dd5ed54124da319ee2e4a8
Ripudaman Singh <rdsingh120@gmail.com>
2024-09-09 15:36:48 -0400

    logo fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,20 @@
 import { FaUserAstronaut } from 'react-icons/fa6'
 import { RxHamburgerMenu } from 'react-icons/rx'
+import { FaCircle } from 'react-icons/fa6'
+
 import './Navbar.css'
 
-const Navbar = ({contactForm, sideMenu}) => {
+const Navbar = ({ contactForm, sideMenu }) => {
   return (
     <div className="navbar">
-      <span className="logo">:Hello World</span>
+      <div className="logo-container">
+        <span className="logo">
+          <FaCircle />
+        </span>
+        <span className="logo">
+          <FaCircle />
+        </span>
+      </div>
       <div className="buttons">
         <button className="contactMe" onClick={contactForm}>
           Contact Me <FaUserAstronaut />
