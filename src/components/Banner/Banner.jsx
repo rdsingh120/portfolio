bc2123288a2ead395e21b7605a7a200f7e3751b9
Ripudaman Singh <rdsingh120@gmail.com>
2024-09-09 02:48:47 -0400

    updated banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,9 +3,9 @@ import './Banner.css'
 const Banner = () => {
   return (
     <div className="banner">
-        <div className="photo">
-            <img src={photo} alt="" />
-        </div>
+      <div className="photo">
+        <img src={photo} alt="" />
+      </div>
     </div>
   )
 }
