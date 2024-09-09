633be0fcaad6ee1c988bfe8fcee98d7c7a580616
Ripudaman Singh <rdsingh120@gmail.com>
2024-09-09 00:04:33 -0400

    updated

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,20 +1,52 @@
 import './ContactForm.css'
 import Title from '../Title/Title'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import toast, { Toaster } from 'react-hot-toast'
+
 const ContactForm = () => {
+  const form = useRef()
+  const notify = () => toast('Message Sent')
+  const serviceId = import.meta.env.VITE_EMAIL_SERVICE_ID
+  const templateId = import.meta.env.VITE_EMAIL_TEMPLATE_ID
+  const publicKey = import.meta.env.VITE_EMAIL_PUBLIC_KEY
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs
+      .sendForm(serviceId, templateId, form.current, {
+        publicKey: publicKey,
+      })
+      .then(
+        () => {
+          notify()
+          console.log('SUCCESS!')
+        },
+        (error) => {
+          console.log('FAILED...', error.text)
+        }
+      )
+    setContact({
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    })
+  }
+
   const [contact, setContact] = useState({
     name: '',
     email: '',
     subject: '',
     message: '',
   })
-
   const updateState = (e) =>
     setContact({ ...contact, [e.target.name]: e.target.value })
 
   const { name, email, subject, message } = contact
   return (
-    <form action="">
+    <form ref={form} onSubmit={sendEmail}>
+      <Toaster />
       <Title title="send me a message" />
       <div className="form-row">
         <input
@@ -23,6 +55,7 @@ const ContactForm = () => {
           placeholder="Name"
           value={name}
           onChange={updateState}
+          required
         />
         <input
           type="email"
@@ -30,6 +63,7 @@ const ContactForm = () => {
           placeholder="Email"
           value={email}
           onChange={updateState}
+          required
         />
       </div>
       <div className="form-row">
@@ -39,6 +73,7 @@ const ContactForm = () => {
           placeholder="Subject"
           value={subject}
           onChange={updateState}
+          required
         />
       </div>
       <div className="form-row">
@@ -48,22 +83,11 @@ const ContactForm = () => {
           className="text-area"
           value={message}
           onChange={updateState}
+          required
         ></textarea>
       </div>
       <div className="form-row">
-        <input
-          className="submit-btn"
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault()
-            setContact({
-              name: '',
-              email: '',
-              subject: '',
-              message: '',
-            })
-          }}
-        />
+        <input className="submit-btn" type="submit" />
       </div>
     </form>
   )
