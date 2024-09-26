98bc2b939ec4223ec9ea1e0c790a7a94021310ff
Ripudaman Singh <rdsingh120@gmail.com>
2024-09-26 00:08:03 -0400

    project updated

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,7 +1,7 @@
 import mernApp from './assets/mern-crud-app.png'
 import movieSearch from './assets/movieSearch.png'
 import vanilla from './assets/vanilla.png'
-import watches from './assets/watches.png'
+import libraryApp from './assets/library-app.png'
 
 export const projects = [
   {
@@ -11,21 +11,22 @@ export const projects = [
     img: mernApp,
   },
   {
+    name: 'Library App',
+    desc: 'React JS | Node JS | Express JS | MongoDB',
+    link: 'https://library-app-bhn5.onrender.com/',
+    img: libraryApp,
+  },
+  {
     name: 'movie search',
     desc: 'React JS | OMDB API | CSS',
     link: 'https://movie-search-app-react-project.netlify.app/',
     img: movieSearch,
   },
+
   {
     name: 'Vanilla JS Mini Projects',
     desc: 'JavaScript | HTML | CSS',
     link: 'https://magical-dasik-545185.netlify.app/',
     img: vanilla,
-  },
-  {
-    name: 'Luxury Watches',
-    desc: 'HTML | CSS',
-    link: 'http://studentweb.cencol.ca/rsin1191/term-project/index.html',
-    img: watches,
   },
 ]
